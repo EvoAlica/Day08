84219357a675a440d2aa1c89672804f5082e21e6
Anela Alic <ane@Tariks-MacBook-Air.local>
2021-03-15 17:23:12 +0100

    Functions

---
+++ ex01/src/index.js
@@ -0,0 +1,7 @@
+function reusableFunction() {
+// Write code below this line
+    console.log("Hi Padawan!");
+}
+reusableFunction();
+// Write code above this line
+module.exports = reusableFunction;
